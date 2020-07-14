0c0940d130b49e5451916ce8f13e9dab9f2e8123
Zola Magama <zolamagama1985@gmail.com>
2020-07-14 15:18:09 +0200

    some more changes

--- reg-number.dom.js
+++ reg-number.dom.js
@@ -3,13 +3,48 @@ const regNumber = document.querySelector(".regNumber");
 const regNumbers = document.querySelector(".regNumbers");
 const addRegNumberBtn = document.querySelector(".addRegNumber");
 const message = document.querySelector(".message");
+const currentTown = document.querySelector(".radioButton");
 
-addRegNumberBtn.addEventListener("click", function() {
+const regNumberEnter = [];
 
+addRegNumberBtn.addEventListener("click", function(e) {
+console.log(e.target.value)
+
+const filteredList = [];
+
+for (var i=0;i<regNumberEntered.length;i++) {
+
+}
 	if (regNumberEntered.value == "") {
 		message.innerHTML = "Please enter a registration number";
 		return;
 	}
+	const currentRegNumber = regNumberEntered.value.toUpperCase();
 
-	regNumber.innerHTML = regNumberEntered.value;
+	if (currentRegNumber.startsWith("CA") ||
+		currentRegNumber.startsWith("CJ") ||
+		currentRegNumber.startsWith("CY")) {
+
+		message.innerHTML = "";
+	
+
+	if (!regNumberEnter.includes(currentRegNumber)) {
+		const regNumberElem = document.createElement("div");
+		regNumberElem.classList.add("regNumber");
+		regNumberElem.innerHTML = currentRegNumber;
+		regNumbers.appendChild(regNumberElem);
+		regNumberEnter.push(currentRegNumber)
+	}
+
+	else {
+		message.innerHTML = "This registration number already exists.";
+	} 
+}
+	else {
+		message.innerHTML = "Invalid registration number entered.";
+	}
+
+
+
+
 });
