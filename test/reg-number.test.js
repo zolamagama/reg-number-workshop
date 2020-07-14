6f8b4c281543bf9f553e5ae49788b4bb98efedf3
avermeulen <andre.vermeulen@gmail.com>
2020-07-14 09:40:48 +0200

    added test folder &  Factory Function skeleton

---
+++ test/reg-number.test.js
@@ -0,0 +1,17 @@
+describe("The Reg Number factory function", function() {
+	
+	it("should add unique reg numbers", function() {
+		assert.equal(1,2);
+	});
+
+	it("should filter for a town", function() {
+		assert.equal(1,2);
+	});
+
+	it("should give error error message for an invalid regNumber", function() {
+		assert.equal(1,2);
+	});
+
+
+
+});
